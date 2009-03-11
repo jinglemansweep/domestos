3eb5d1334c08e50b06737854200828f5cb3505bd
Louis King <louis@neutrino.(none)>
2009-03-11 15:17:59 +0000

    Updated X10 Heyu client

--- clients/x10heyu/receiver.py
+++ clients/x10heyu/receiver.py
@@ -7,14 +7,27 @@ import sys
 import time
 from memcache import Client
 
-msg_client = Client(["10.0.2.10:21122",])
+# Configuration
+
+MSG_HOSTS = ["127.0.0.1:21122",]
+HEYU2_PATH = "/usr/local/bin/heyu2"
+
+# Main Loop
+
+msg_client = Client(MSG_HOSTS)
 
 while True:
 
     msg = msg_client.get("domestos.output")
 
     if msg:
-        print msg
+        plugin = msg["plugin"]
+        payload = msg["payload"]
+        house, unit, command = payload["house"], payload["unit"], payload["command"]
+        if all([house, unit, command]):
+                heyu_opts = "%s %s%s" % (command, house, unit)
+                print heyu_opts
+                subprocess.Popen("%s %s"  % (HEYU2_PATH, heyu_opts), shell=True)
     
     else:
         time.sleep(1)
